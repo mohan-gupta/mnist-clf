ef34fa56346329948e5ba1c6e7907688b2f4b58b
Mohan Gupta <mohankrishnagupta@gmail.com>
2024-01-24 03:08:47 +0530

    removed mysql connection

--- app/main.py
+++ app/main.py
@@ -1,8 +1,4 @@
-import os
-from dotenv import load_dotenv
 from contextlib import asynccontextmanager
-
-import MySQLdb
 
 from fastapi import FastAPI, UploadFile
 
@@ -11,32 +7,7 @@ import cv2
 
 from app.predict import get_model, predict
 
-def setup_db():
-    # Database configuration
-    db_config = {
-        'host': os.getenv("MYSQL_HOST"),
-        'user': os.getenv("MYSQL_USER"),
-        'passwd': os.getenv("MYSQL_PASS"),
-        'db': os.getenv("MYSQL_DB"),
-    }
-
-    # Create a connection to the database
-    conn = MySQLdb.connect(**db_config)
-    
-    create_table = """
-    CREATE TABLE [IF NOT EXISTS] logs(
-        image VARCHAR(255) NOT NULL,
-        prediction INT NOT NULL
-    )"""
-    cursor = conn.cursor()
-    cursor.execute(create_table)
-    conn.commit()
-    cursor.close()
-    
-    return conn
-
 model_dct = {}
-mysql_conn = setup_db()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -54,8 +25,6 @@ def home():
 
 @app.post("/predict")
 async def pred(image: UploadFile):
-    global mysql_conn
-    
     # read the image
     contents = await image.read()
     np_img = np.fromstring(contents, np.uint8)
@@ -66,14 +35,6 @@ async def pred(image: UploadFile):
     img = np.expand_dims(img, axis=2)
     
     pred = predict(model_dct['model'], img.copy())
-    
-    cursor = mysql_conn.cursor()
-    query = "INSERT INTO logs (image, prediction) VALUES (%s, %s)"
-    
-    cursor.execute(query, (contents, pred))
-    mysql_conn.commit()
-    cursor.close()
-    
     return {
         "response": pred
     }
